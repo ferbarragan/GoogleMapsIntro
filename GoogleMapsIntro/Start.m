7de29b58687e063e192c90cee86a9fbef5d613f5
ferbarragan <ferbarragan.developer@gmail.com>
2016-07-02 13:02:56 -0500

    Basic Google Maps API configuration added.

--- GoogleMapsIntro/Start.m
+++ GoogleMapsIntro/Start.m
@@ -7,16 +7,35 @@
 //
 
 #import "Start.h"
+@import GoogleMaps;
 
 @interface Start ()
 
 @end
 
-@implementation Start
+@implementation Start {
+    GMSMapView *mapView_;
+}
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    //[super viewDidLoad];
+    
+    // Create a GMSCameraPosition that tells the map to display the
+    // coordinate -33.86,151.20 at zoom level 6.
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:20.606967
+                                                            longitude:-103.4180457
+                                                                 zoom:15];
+    mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+    mapView_.myLocationEnabled = YES;
+    self.view = mapView_;
+    
+    // Creates a marker in the center of the map.
+    GMSMarker *marker = [[GMSMarker alloc] init];
+    marker.position = CLLocationCoordinate2DMake(20.606967,-103.4180457);
+    marker.title = @"ITESO";
+    marker.snippet = @"Universidad Jesuita de Guadalajara";
+    marker.map = mapView_;
+    
 }
 
 - (void)didReceiveMemoryWarning {
---
+++ Podfile
@@ -0,0 +1,6 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.1'
+use_frameworks!
+target 'GoogleMapsIntro' do
+pod 'GoogleMaps'
+end
